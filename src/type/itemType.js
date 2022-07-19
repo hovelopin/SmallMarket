0be206aaebbfdaa6e866b89ae461bd07db5c0b8a
foxmon <jjockrod@naver.com>
2022-07-19 19:27:53 +0900

    itemType init

---
+++ src/type/itemType.js
@@ -0,0 +1,20 @@
+import NameUtil from "../util/nameUtil"
+import ErrorUtil from "../util/errorUtil"
+
+class ItemType extends NameUtil {
+    constructor(name, price, quantity, img, type) {
+        super(type)
+        ErrorUtil.invalidParameter(name)
+        this.name = name
+        ErrorUtil.invalidParameter(price)
+        this.price = price
+        ErrorUtil.invalidParameter(quantity)
+        this.quantity = quantity
+        ErrorUtil.invalidParameter(img)
+        this.img = img
+
+        NameUtil.freezeObject(this, ItemType)
+    }
+}
+
+export default ItemType
