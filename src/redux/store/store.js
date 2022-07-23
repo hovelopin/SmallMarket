cfdc8a3b2758d7e4d089cdd94778011724aca052
foxmon <jjockrod@naver.com>
2022-07-23 14:08:24 +0900

    store format 수정

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,20 +1,22 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { authReducer } from '../reducers/authReducer';
-import { cartReducer } from '../reducers/cartReducer';
-import { getItemReducer, getItemDetailReducer } from '../reducers/itemReducer';
-import thunk from 'redux-thunk';
+import { createStore, combineReducers, applyMiddleware } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+import { authReducer } from "../reducers/authReducer"
+import { cartReducer } from "../reducers/cartReducer"
+import { getItemReducer, getItemDetailReducer } from "../reducers/itemReducer"
+import thunk from "redux-thunk"
 
 const reducer = combineReducers({
     user: authReducer,
     cart: cartReducer,
     getItems: getItemReducer,
     getItemDetails: getItemDetailReducer,
-});
+})
 
-const middleware = [thunk];
+const middleware = [thunk]
 
-const cartLocalStorage = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+const cartLocalStorage = localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart"))
+    : []
 
 const INIT_STATE = {
     cart: {
@@ -24,8 +26,8 @@ const INIT_STATE = {
 
 const store = createStore(
     reducer,
-    INIT_STATE, 
-    composeWithDevTools(applyMiddleware(...middleware)),
-);
+    INIT_STATE,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
 
-export default store;
+export default store
