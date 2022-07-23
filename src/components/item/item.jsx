35ceb2b7ca6706aebba43a8e1214feb0aa2e3a27
foxmon <jjockrod@naver.com>
2022-07-23 14:09:10 +0900

    item Fix

--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -1,34 +1,27 @@
-import React from "react";
-import { useHistory } from 'react-router-dom';
-import styles from './item.module.css';
+import React from "react"
+import { useHistory } from "react-router-dom"
+import styles from "./item.module.css"
 
-const Item = ({ id, name, price, quantity, img }) => {
-  const history = useHistory();
-  const base = '/img/items/';
-  const imgRoute = base + `${img}`;
-  const TOKEN = 'token';
-  const buttonClickHandler = () => {
-    if(localStorage.getItem(TOKEN)) {
-      history.push(`/items/detail/${id}`);
-    } else {
-      alert('You need to login...');
+const Item = ({ uuid, name, price, quantity, img }) => {
+    const history = useHistory()
+
+    const buttonClickHandler = () => {
+        history.push(`/items/detail/${uuid}`)
     }
-  }
 
-  return (
-      <section className={styles.itemBox}>
-        <img className={styles.imgSet} src={imgRoute} alt={name} />
-        <div className={styles.itemInfo}>
-          <h2 className={styles.name}>{name}</h2>
-          <p className={styles.itemPrice}>Price : {price}</p>
-          <p className={styles.itemQuantity}>Quantity : {quantity}</p>
-          <p className={styles.description}>Description : {name}</p>
-          <button 
-            className={styles.button}
-            onClick={buttonClickHandler}
-          >Detail</button>
-        </div>
-      </section>    
-  );
+    return (
+        <section className={styles.itemBox}>
+            <img className={styles.imgSet} src={img} alt={name} />
+            <div className={styles.itemInfo}>
+                <h2 className={styles.name}>{name}</h2>
+                <p className={styles.itemPrice}>Price : {price}</p>
+                <p className={styles.itemQuantity}>Quantity : {quantity}</p>
+                <p className={styles.description}>Description : {name}</p>
+                <button className={styles.button} onClick={buttonClickHandler}>
+                    Detail
+                </button>
+            </div>
+        </section>
+    )
 }
-export default Item;
+export default Item
