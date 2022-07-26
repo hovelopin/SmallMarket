0e0ffb6721d579335e339be3675303cef0eba033
foxmon <jjockrod@naver.com>
2022-07-26 20:45:40 +0900

    Refactor: register component change

--- src/components/login/register.jsx
+++ src/components/login/register.jsx
@@ -1,133 +1,119 @@
 import React, { useState, useCallback } from "react"
 import { useDispatch } from "react-redux"
-import { useHistory } from "react-router"
+import { Container, Box, Input, Button, Typography } from "@mui/material"
 import { registerRequest } from "../../redux/action/authAction"
-import styles from "./register.module.css"
-import { Link } from "react-router-dom"
 
 function Register() {
-    const dispatch = useDispatch()
     const [username, setUserName] = useState("")
     const [password, setPassword] = useState("")
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [checkPasswrod, setCheckPassword] = useState("")
-    const history = useHistory()
 
-    const userNameChangeHandler = useCallback((event) => {
+    const dispatch = useDispatch()
+
+    const onUserNameChangeEventHandler = useCallback((event) => {
         setUserName(event.target.value)
     }, [])
 
-    const passwordChangeHandler = useCallback((event) => {
+    const onPasswordChangeEventHandler = useCallback((event) => {
         setPassword(event.target.value)
     }, [])
 
-    const checkPasswordChangeHandler = useCallback((event) => {
+    const onCheckPasswordChangeEventHandler = useCallback((event) => {
         setCheckPassword(event.target.value)
     }, [])
 
-    const nameChangeHandler = useCallback((event) => {
+    const onNameChangeEventHandler = useCallback((event) => {
         setName(event.target.value)
     }, [])
 
-    const emailChangeHandler = useCallback((event) => {
+    const onEmailChangeEventHandler = useCallback((event) => {
         setEmail(event.target.value)
     }, [])
 
-    const onSubmitHandler = (event) => {
+    const onSubmiButtonClickEventtHandler = (event) => {
         event.preventDefault()
         if (password === checkPasswrod) {
             dispatch(registerRequest(username, password, email, name))
-            if (window.confirm("Success Register! Do you want to Shop?")) {
-                history.push("/login")
-            } else {
-                history.push("/")
-            }
-        } else {
-            alert("Please check your password...")
         }
     }
 
     return (
-        <form onSubmit={onSubmitHandler}>
-            <Link to="/main">
-                <div>
-                    <img
-                        className={styles.intro}
-                        src="/img/reg_smallmarket.png"
-                        alt="reg_intro"
-                    />
-                </div>
-            </Link>
-            <div className={styles.wapper}>
-                <div className={styles.content}>
-                    <div>
-                        <h3>ID</h3>
-                        <span className={styles.box_id}>
-                            <input
-                                className={styles.int}
-                                placeholder="Enter ID"
-                                onChange={userNameChangeHandler}
-                                required
-                            ></input>
-                        </span>
-                    </div>
-                    <div>
-                        <h3>PASSWORD</h3>
-                        <span className={styles.box_id}>
-                            <input
-                                type="password"
-                                minLength="8"
-                                className={styles.int}
-                                placeholder="Enter password"
-                                onChange={passwordChangeHandler}
-                                required
-                            ></input>
-                        </span>
-                    </div>
-                    <div>
-                        <h3>CONFIRM PASSWORD</h3>
-                        <span className={styles.box_id}>
-                            <input
-                                type="password"
-                                className={styles.int}
-                                placeholder="Confirm password"
-                                onChange={checkPasswordChangeHandler}
-                                required
-                            ></input>
-                        </span>
-                    </div>
-                    <div>
-                        <h3>E-MAIL</h3>
-                        <span className={styles.box_id}>
-                            <input
-                                className={styles.int}
-                                type="email"
-                                placeholder="ex) sungkonghoe@skhu.ac.kr"
-                                onChange={emailChangeHandler}
-                                required
-                            ></input>
-                        </span>
-                    </div>
-                    <div>
-                        <h3>NICK NAME</h3>
-                        <span className={styles.box_id}>
-                            <input
-                                className={styles.int}
-                                placeholder="Your name in SmallMarket"
-                                onChange={nameChangeHandler}
-                                required
-                            ></input>
-                        </span>
-                    </div>
-                    <div className={styles.btn}>
-                        <button className={styles.btnJoin} type="submit">
-                            <span>Ready to Jump</span>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </form>
+        <Container>
+            <Box sx={boxStyle}>
+                <img
+                    style={imgStyle}
+                    src="/img/reg_smallmarket.png"
+                    alt="reg_intro"
+                />
+            </Box>
+            <Box sx={contentBoxStyle}>
+                <Typography variant="h4" align="center">
+                    REGISTER
+                </Typography>
+                <Input
+                    sx={inputStyle}
+                    placeholder="Enter ID"
+                    onChange={onUserNameChangeEventHandler}
+                    required
+                />
+                <Input
+                    type="password"
+                    sx={inputStyle}
+                    placeholder="Enter password"
+                    onChange={onPasswordChangeEventHandler}
+                    required
+                />
+                <Input
+                    type="password"
+                    sx={inputStyle}
+                    placeholder="Confirm password"
+                    onChange={onCheckPasswordChangeEventHandler}
+                    required
+                />
+                <Input
+                    type="email"
+                    sx={inputStyle}
+                    placeholder="smallmarket@gmail.com"
+                    onChange={onEmailChangeEventHandler}
+                    required
+                />
+                <Input
+                    sx={inputStyle}
+                    placeholder="Your name in SmallMarket"
+                    onChange={onNameChangeEventHandler}
+                    required
+                />
+                <Button onClick={onSubmiButtonClickEventtHandler}>
+                    Ready to Shopping
+                </Button>
+            </Box>
+        </Container>
     )
 }
+
+const boxStyle = {
+    mt: 15,
+    textAlign: "center",
+}
+
+const imgStyle = {
+    width: "240px",
+}
+
+const contentBoxStyle = {
+    width: "70%",
+    background: "white",
+    boxShadow: "0 2px 4px 0 rgba(0, 0, 0, 0.2)",
+    margin: "0 auto",
+    mt: 1,
+}
+
+const inputStyle = {
+    width: "90%",
+    display: "block",
+    m: 2,
+}
+
 export default Register
