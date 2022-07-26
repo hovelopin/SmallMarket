56d0b59e5b77e2983fa7852d443f7643c59efee4
foxmon <jjockrod@naver.com>
2022-07-26 20:21:03 +0900

    Refactor: authReducer format 수정

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,31 +1,31 @@
 import {
-  REGISTER_USER,
-  USER_LOGIN,
-  REGISTER_ERROR,
-  USER_LOGIN_FAIL
-} from '../actionTypes/auth/authActionTypes';
+    REGISTER_USER,
+    USER_LOGIN,
+    REGISTER_ERROR,
+    USER_LOGIN_FAIL,
+} from "../actionTypes/auth/authActionTypes"
 
 export const authReducer = (state = { user: {} }, action) => {
-  switch(action.type) {
-    case REGISTER_USER:
-      return { 
-        ...state, 
-        register: action.payload, 
-      }
+    switch (action.type) {
+        case REGISTER_USER:
+            return {
+                ...state,
+                register: action.payload,
+            }
 
-    case USER_LOGIN:
-      return { 
-        ...state, 
-        login: action.payload,
-      }
+        case USER_LOGIN:
+            return {
+                ...state,
+                login: action.payload,
+            }
 
-    case REGISTER_ERROR:
-      return { error: action.payload }
+        case REGISTER_ERROR:
+            return { error: action.payload }
 
-    case USER_LOGIN_FAIL:
-      return { error: action.payload }
+        case USER_LOGIN_FAIL:
+            return { error: action.payload }
 
-    default:
-      return state;
-  }
+        default:
+            return state
+    }
 }
