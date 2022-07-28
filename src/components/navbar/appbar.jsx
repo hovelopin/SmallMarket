bae17c8895a29a81cc78e11abd2b16e0409750e6
foxmon <jjockrod@naver.com>
2022-07-28 19:58:17 +0900

    Refactor: appbar 수정

--- src/components/navbar/appbar.jsx
+++ src/components/navbar/appbar.jsx
@@ -1,19 +1,29 @@
-import React from "react"
-import { Box, AppBar, Toolbar, Typography, Button } from "@mui/material"
+import React, { useState } from "react"
+import {
+    Box,
+    AppBar,
+    Toolbar,
+    Typography,
+    Button,
+    Menu,
+    MenuItem,
+} from "@mui/material"
 import Icon from "../../icon/icon"
 
 function Appbar() {
-    // onClickEventHandler를 공통화 한다
+    const [itemAnchorEl, setItemAnchorEl] = useState(null)
+
+    const isOpen = Boolean(itemAnchorEl)
+
+    // onClick 함수가 있을 경우에만 동작하도록 한다.
     const items = [
         {
             name: "SHOP",
             icon: <Icon name="bag" />,
             path: "/items",
-        },
-        {
-            name: "CONTACT",
-            icon: <Icon name="contact" />,
-            path: "/contact",
+            onClick: (e) => {
+                setItemAnchorEl(e.currentTarget)
+            },
         },
         {
             name: "CART",
@@ -30,22 +40,41 @@ function Appbar() {
             icon: <Icon name="signup" />,
             path: "/register",
         },
-        {
-            name: "Q & A",
-            icon: <Icon name="qna" />,
-            path: "/board",
-        },
-        {
-            name: "ABOUT",
-            icon: <Icon name="about" />,
-            path: "/about",
-        },
     ]
 
+    const category = [
+        { name: "Vegetable" },
+        { name: "Meat" },
+        { name: "Drink" },
+        { name: "Normal" },
+    ]
+
+    const onMenuButtonClickEventHandler = (menuName) => () => {
+        // TODO: menuName에 따른 라우팅
+        // app.jsx에서 params로 분류
+        // 카테고리 이름에 따라서 item_list에서 fetch를 할 것
+        switch (menuName) {
+            case "Vegetable":
+                break
+            case "Meat":
+                break
+            case "Drink":
+                break
+            case "Normal":
+                break
+            default:
+                return
+        }
+    }
+
+    const onMenuCloseButtonClickEventHandler = () => {
+        setItemAnchorEl(null)
+    }
+
     return (
-        <Box sx={boxStyle}>
+        <React.Fragment>
             <AppBar component="nav">
-                <Toolbar>
+                <Toolbar sx={boxStyle}>
                     <Box sx={menuStyle}>
                         {items.map((item) => (
                             <Box key={item.name} sx={navStyle}>
@@ -60,12 +89,29 @@ function Appbar() {
                     </Box>
                 </Toolbar>
             </AppBar>
-        </Box>
+            <Menu
+                anchorEl={itemAnchorEl}
+                open={isOpen}
+                onClose={onMenuCloseButtonClickEventHandler}
+            >
+                {category.map((c) => (
+                    <MenuItem
+                        key={c.name}
+                        onClick={onMenuButtonClickEventHandler(c.name)}
+                    >
+                        {c.name}
+                    </MenuItem>
+                ))}
+            </Menu>
+        </React.Fragment>
     )
 }
 
 const boxStyle = {
     display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: 1,
 }
 
 const menuStyle = {
