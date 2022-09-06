8123a7aa16eab3d3ec789516cb269af25f1e9421
foxmon <jjockrod@naver.com>
2022-09-06 23:48:24 +0900

    app

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,16 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Header from "./components/UI/template/header"
 
 function App() {
     return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={<div></div>} />
-            </Switch>
-        </Router>
+        <Header />
+        // <Router>
+        //     <Switch>
+        //         <Route exact path="/" component={Test} />
+        //         <Route exact path="/test" component={Test2} />
+        //     </Switch>
+        // </Router>
     )
 }
 
