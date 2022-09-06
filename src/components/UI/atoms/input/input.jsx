7812c88fd195e81aa80014dbb53eae8a805cef20
foxmon <jjockrod@naver.com>
2022-09-06 13:20:00 +0900

    Fix: input 수정

--- src/components/UI/atoms/input/input.jsx
+++ src/components/UI/atoms/input/input.jsx
@@ -1,13 +1,13 @@
 import StyledInput from "./style"
 
-const Input = ({ width, height, placeholder, type, onChange }) => {
+const Input = ({ width, height, placeholder, type, onChangeEvent }) => {
     return (
         <StyledInput
             width={width}
             height={height}
             placeholder={placeholder}
             type={type}
-            onChange={onChange}
+            onChange={onChangeEvent}
         />
     )
 }
