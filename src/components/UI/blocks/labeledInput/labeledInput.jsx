6822deeef92896f402ff78b17635c4ec126a33e2
foxmon <jjockrod@naver.com>
2022-09-06 21:12:42 +0900

    Fix: 폴더이름 오타 수정

---
+++ src/components/UI/blocks/labeledInput/labeledInput.jsx
@@ -0,0 +1,14 @@
+import Container from "../../atoms/container/container"
+import Label from "../../atoms/label/label"
+import Input from "../../atoms/input/input"
+
+const LabeledInput = ({ width, labelText, placeholder }) => {
+    return (
+        <Container width={width} display="block">
+            <Label labelText={labelText} />
+            <Input type="text" placeholder={placeholder} />
+        </Container>
+    )
+}
+
+export default LabeledInput
