5ad741c1411c36c624701933f6705fffa11c35a2
hovelopin <kimhojin3714@naver.com>
2022-09-07 16:39:19 +0900

    feat : button atoms 완성

--- src/components/UI/atoms/button/Button.jsx
+++ src/components/UI/atoms/button/Button.jsx
@@ -1,5 +1,12 @@
-const Button = () => {
-    return <button></button>
+import React from "react"
+import StyledButton from "./style"
+
+const Button = ({ height, width }) => {
+    return (
+        <StyledButton width={width} height={height}>
+            안녕
+        </StyledButton>
+    )
 }
 
 export default Button
--- src/components/UI/atoms/button/style.js
+++ src/components/UI/atoms/button/style.js
@@ -1,7 +1,18 @@
 import styled from "styled-components"
+import Theme from "../../../../util/style/theme"
 
 const StyledButton = styled.button`
     color: #fff;
+    text-align: center;
+    height: ${(props) => props.height};
+    width: ${(props) => props.width};
+    border-radius: ${Theme.buttons.borderRadius};
+    padding: ${Theme.buttons.padding};
+    border: ${Theme.buttons.border};
+    background-color: ${Theme.buttons.background};
+    font-size: ${Theme.fontSizes.default};
+    font-weight: ${Theme.fontWeight.medium};
+    cursor: pointer;
 `
 
 export default StyledButton
