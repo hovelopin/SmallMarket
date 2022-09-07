1a7c55138b56087379b7fb028b6a3d09d6d722ac
foxmon <jjockrod@naver.com>
2022-09-07 22:54:58 +0900

    Fix: App customRouter 추가

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,13 @@
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import React from "react"
-import MainPage from "./components/UI/container/mainPage"
+import CustomRouter from "./components/router/customRouter"
 import Footer from "./components/UI/container/footer"
 
 function App() {
     return (
         <React.Fragment>
-            <MainPage />
+            <CustomRouter />
             <Footer />
         </React.Fragment>
-        // <Router>
-        //     <Switch>
-        //         <Route exact path="/" component={Test} />
-        //         <Route exact path="/test" component={Test2} />
-        //     </Switch>
-        // </Router>
     )
 }
 
