be5712d1a49af8db60be574237080d5ecae277f0
foxmon <jjockrod@naver.com>
2022-09-07 22:51:35 +0900

    Fix: App setting

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,14 @@
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import React from "react"
 import MainPage from "./components/UI/container/mainPage"
+import Footer from "./components/UI/container/footer"
 
 function App() {
     return (
-        <MainPage />
+        <React.Fragment>
+            <MainPage />
+            <Footer />
+        </React.Fragment>
         // <Router>
         //     <Switch>
         //         <Route exact path="/" component={Test} />
