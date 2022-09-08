59ed49300ef6768e9525f0dbefbe04e4db0d876a
vpvm96 <vpvm96@gmail.com>
2022-09-08 17:56:04 +0900

    FIx: app 에 header추가

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import React from "react"
+import Header from "./components/UI/container/header"
 import CustomRouter from "./components/router/customRouter"
 import Footer from "./components/UI/container/footer"
 
 function App() {
     return (
         <React.Fragment>
+            <Header />
             <CustomRouter />
             <Footer />
         </React.Fragment>
