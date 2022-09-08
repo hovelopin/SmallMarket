b7f12f0992c4ee03463d66ab60a74ff3d66bfa96
foxmon <jjockrod@naver.com>
2022-09-08 13:48:26 +0900

    Fix: Button 수정

--- src/components/UI/atoms/button/button.jsx
+++ src/components/UI/atoms/button/button.jsx
@@ -1,12 +1,38 @@
 import React from "react"
-import StyledButton from "./style"
+import * as MyButton from "./style"
+import ErrorUtil from "../../../../util/errorUtil"
 
-const Button = ({ height, width, value, onClickEvent }) => {
-    return (
-        <StyledButton width={width} height={height} onClick={onClickEvent}>
-            {value}
-        </StyledButton>
-    )
+const Button = ({ type = "default", height, width, value, onClickEvent }) => {
+    const getButtonType = (type) => {
+        switch (type) {
+            case "default":
+                return (
+                    <MyButton.StyledButton
+                        width={width}
+                        height={height}
+                        onClick={onClickEvent}
+                    >
+                        {value}
+                    </MyButton.StyledButton>
+                )
+
+            case "contrast":
+                return (
+                    <MyButton.StyledContrastButton
+                        width={width}
+                        height={height}
+                        onClick={onClickEvent}
+                    >
+                        {value}
+                    </MyButton.StyledContrastButton>
+                )
+
+            default:
+                ErrorUtil.notImplemented()
+        }
+    }
+
+    return <React.Fragment>{getButtonType(type)}</React.Fragment>
 }
 
 export default Button
