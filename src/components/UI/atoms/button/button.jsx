9f5e66c4a5189367393872d2114ace5ab0a0b1aa
foxmon <jjockrod@naver.com>
2022-09-08 12:54:52 +0900

    Fix: click event

--- src/components/UI/atoms/button/button.jsx
+++ src/components/UI/atoms/button/button.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import StyledButton from "./style"
 
-const Button = ({ height, width, value }) => {
+const Button = ({ height, width, value, onClickEvent }) => {
     return (
-        <StyledButton width={width} height={height}>
+        <StyledButton width={width} height={height} onClick={onClickEvent}>
             {value}
         </StyledButton>
     )
--- src/components/UI/blocks/searchBar/searchBar.jsx
+++ src/components/UI/blocks/searchBar/searchBar.jsx
@@ -2,13 +2,23 @@ import Container from "../../atoms/container/container"
 import Input from "../../atoms/input/input"
 import Button from "../../atoms/button/button"
 
-const SearchBar = ({ width, placeholder, value, onChangeEvent }) => {
-	return (
-		<Container width={width} display="flex">
-			<Input tpye="text" placeholder={placeholder} onChangeEvent={onChangeEvent} />
-			<Button width="auto" value={value} />
-		</Container>
-	)
+const SearchBar = ({
+    width,
+    placeholder,
+    value,
+    onChangeEvent,
+    onClickEvent,
+}) => {
+    return (
+        <Container width={width} display="flex">
+            <Input
+                tpye="text"
+                placeholder={placeholder}
+                onChangeEvent={onChangeEvent}
+            />
+            <Button width="auto" value={value} onClickEvent={onClickEvent} />
+        </Container>
+    )
 }
 
 export default SearchBar
