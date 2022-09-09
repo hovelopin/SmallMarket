305e6c33e61ac3dc38ac5936c61fbdc36818a04d
Velo248 <jhpark4884@gmail.com>
2022-09-10 02:16:22 +0900

    customRouter 원상복구

--- src/components/router/customRouter.jsx
+++ src/components/router/customRouter.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Main from "@components/UI/container/main"
 import LoginContainer from "@components/UI/container/loginContainer"
-import Carousel from "../UI/blocks/carousel/carousel"
 const CustomRouter = () => {
     return (
         <Router>
             <Switch>
-                <Route exact path="/" component={Carousel} />
+                <Route exact path="/" component={Main} />
                 <Route exact path="/login" component={LoginContainer} />
             </Switch>
         </Router>
