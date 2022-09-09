945c3f9994c161a04ba213de2ac05d1e1f0870bd
foxmon <jjockrod@naver.com>
2022-09-09 11:24:10 +0900

    Fix: InfoHeader fix

--- src/components/UI/blocks/info/infoHeader.jsx
+++ src/components/UI/blocks/info/infoHeader.jsx
@@ -2,30 +2,55 @@ import styled from "styled-components"
 import Grid from "@components/UI/atoms/grid/grid"
 import Container from "@components/UI/atoms/container/container"
 import Text from "@components/UI/atoms/text/text"
+import SearchBar from "@components/UI/blocks/searchBar/searchBar"
 import Theme from "@util/style/theme"
 
 const InfoHeader = ({ headerItems }) => {
     return (
-        <StyledWrapper>
-            <Grid
-                repeat={5}
-                axis="column"
-                bgColor={Theme.colors.black}
-                color={Theme.colors.white}
-                border="8px"
-            >
-                {headerItems.map((h) => (
-                    <Container key={h.context}>
-                        <Text type={h.type} context={h.context} />
-                    </Container>
-                ))}
-            </Grid>
-        </StyledWrapper>
+        <Container width="100%">
+            <StyledContainer>
+                <Container>
+                    <Text type="large" context="Search" />
+                </Container>
+                <Container>
+                    <SearchBar
+                        width="100%"
+                        placeholder="Search user..."
+                        value="Search"
+                    />
+                </Container>
+            </StyledContainer>
+            <StyledWrapper>
+                <Grid
+                    repeat={5}
+                    axis="column"
+                    bgColor={Theme.colors.black}
+                    color={Theme.colors.white}
+                    border="8px"
+                >
+                    {headerItems.map((h) => (
+                        <Container key={h.context}>
+                            <Text type={h.type} context={h.context} />
+                        </Container>
+                    ))}
+                </Grid>
+            </StyledWrapper>
+        </Container>
     )
 }
 
+const StyledContainer = styled.div`
+    width: 80%;
+    margin: 0 auto;
+    margin-top: 3rem;
+    justify-content: space-between;
+    display: flex;
+    flex: 1;
+`
+
 const StyledWrapper = styled.div`
     width: 100%;
+    margin-top: 2rem;
     display: grid;
     place-items: center;
 `
