c31252e7b509bba13b92e2161529db2ea02230c4
foxmon <jjockrod@naver.com>
2022-09-10 00:10:47 +0900

    Fix: Validation username 추가

--- src/util/validation/validation.js
+++ src/util/validation/validation.js
@@ -1,6 +1,11 @@
 class Validation {
+    static validateUsername(username) {
+        if (isIncludesSpace(username)) return false
+        return username.length > 4 ? true : false
+    }
+
     static validateEmail(email) {
-        if (email.includes(" ")) return false
+        if (isIncludesSpace(email)) return false
         const isValid = email.match(
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         )
@@ -9,13 +14,18 @@ class Validation {
     }
 
     static validatePassword(password) {
-        if (password.includes(" ")) return false
+        if (isIncludesSpace(password)) return false
         const isValid = password.match(
             /^[A-Za-z0-9`~!@#$%^&*|\\\'\";:\/?]{8,15}$/
         )
         if (!isValid) return false
         return true
     }
+}
+
+function isIncludesSpace(str) {
+    if (str.includes(" ")) return true
+    return false
 }
 
 Object.freeze(Validation)
