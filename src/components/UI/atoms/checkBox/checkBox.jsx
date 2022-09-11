cf9f7e13a1c27f329cc0ac2e7c55e82c8c6f1ab5
vpvm96 <vpvm96@gmail.com>
2022-09-11 21:33:10 +0900

    Add: checkBoxStyle 추가

--- src/components/UI/atoms/checkBox/checkBox.jsx
+++ src/components/UI/atoms/checkBox/checkBox.jsx
@@ -1,13 +1,9 @@
-import styled from "styled-components"
+import StyledCheckBox from "./style"
 
 const CheckBox = ({ checked, type, onClickEvnet }) => {
     return (
         <StyledCheckBox checked={checked} type={type} onClick={onClickEvnet} />
     )
 }
-
-const StyledCheckBox = styled.input`
-    transform: scale(1.5);
-`
 
 export default CheckBox
---
+++ src/components/UI/atoms/checkBox/style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+const StyledCheckBox = styled.input`
+    transform: scale(1.5);
+`
+
+export default StyledCheckBox
