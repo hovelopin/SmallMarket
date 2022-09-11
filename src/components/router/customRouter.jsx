003fd2032aaf401e4177b1cf30ea47ce632fe52e
foxmon <jjockrod@naver.com>
2022-09-11 19:14:29 +0900

    Fix: customRouter 수정

--- src/components/router/customRouter.jsx
+++ src/components/router/customRouter.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Main from "@components/UI/container/main"
 import LoginContainer from "@components/UI/container/loginContainer"
+import AdminContainer from "@components/UI/container/adminContainer"
+
 const CustomRouter = () => {
     return (
         <Router>
             <Switch>
                 <Route exact path="/" component={Main} />
                 <Route exact path="/login" component={LoginContainer} />
+                <Route exact path="/admin" component={AdminContainer} />
             </Switch>
         </Router>
     )
