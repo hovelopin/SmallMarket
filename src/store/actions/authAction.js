41d1327f6b76758e7a1f118ef032812d83ebe08b
foxmon <jjockrod@naver.com>
2022-09-12 23:04:29 +0900

    Fix: authAction 로직 추가

--- src/store/actions/authAction.js
+++ src/store/actions/authAction.js
@@ -1,1 +1,17 @@
-export const authRegisterReuqestAction = () => {}
+import Data from "@/dev/data"
+
+export const authLoginReuqestAction = (email, password) => async (dispatch) => {
+    try {
+        const data = await Data.loginRequest(email, password)
+        dispatch({
+            type: "REQUEST_USER_LOGIN",
+            payload: data,
+        })
+        return data
+    } catch (e) {
+        dispatch({
+            type: "REQUEST_USER_LOGIN_FAIL",
+            payload: e.message,
+        })
+    }
+}
