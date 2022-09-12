02befeb5e463317f6e153ccd0595482cede06a38
foxmon <jjockrod@naver.com>
2022-09-12 23:04:22 +0900

    Fix: authReducer 수정

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -10,6 +10,17 @@ const INITIAL_STATE = {
 
 const authReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case "REQUEST_USER_LOGIN":
+            return {
+                ...state,
+                user: action.payload,
+            }
+
+        case "REQUEST_USER_LOGIN_FAIL":
+            return {
+                error: action.payload,
+            }
+
         default:
             return state
     }
