7a221268eb65e06400190922d50c176d3aed7e4e
foxmon <jjockrod@naver.com>
2022-09-13 17:51:58 +0900

    Feat: authActionType 생성

--- src/store/actions/authAction.js
+++ src/store/actions/authAction.js
@@ -1,16 +1,22 @@
+import UserActionTypes from "@/store/types/userActionTypes"
 import Data from "@/dev/data"
+
+export const authUserStateRequestAction = () => async (dispatch) => {
+    dispatch({
+        type: UserActionTypes.REQUEST_USER_STATE,
+    })
+}
 
 export const authLoginReuqestAction = (email, password) => async (dispatch) => {
     try {
         const data = await Data.loginRequest(email, password)
         dispatch({
-            type: "REQUEST_USER_LOGIN",
+            type: UserActionTypes.REQUEST_USER_LOGIN,
             payload: data,
         })
-        return data
     } catch (e) {
         dispatch({
-            type: "REQUEST_USER_LOGIN_FAIL",
+            type: UserActionTypes.REQUEST_USER_LOGIN_FAIL,
             payload: e.message,
         })
     }
