98c18271c736821166785ef5f211b7e10df5a943
foxmon <jjockrod@naver.com>
2022-09-15 21:28:19 +0900

    Fix: Grid 수정

--- src/components/UI/atoms/grid/grid.jsx
+++ src/components/UI/atoms/grid/grid.jsx
@@ -1,10 +1,20 @@
 import StyledGrid from "./style"
 
-const Grid = ({ repeat, axis, bgColor, color, border, shadow, children }) => {
+const Grid = ({
+    repeat,
+    axis,
+    gap,
+    bgColor,
+    color,
+    border,
+    shadow,
+    children,
+}) => {
     return (
         <StyledGrid
             repeat={repeat}
             axis={axis}
+            gap={gap}
             bgColor={bgColor}
             color={color}
             border={border}
