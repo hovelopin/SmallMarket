1223b5fb2168195f39531ceebc75fe648f6381dc
Velo248 <jhpark4884@gmail.com>
2022-09-15 23:05:06 +0900

    payment blocks 수정

--- src/components/UI/blocks/payment/paymentDetail.jsx
+++ src/components/UI/blocks/payment/paymentDetail.jsx
@@ -1,100 +1,115 @@
-import Label from "../../atoms/label/label"
-import Text from "../../atoms/text/text"
-import LabeledInput from "../labeledInput/labeledInput"
 import styled from "styled-components"
-import Button from "../../atoms/button/button"
+import Label from "@components/UI/atoms/label/label"
+import Text from "@components/UI/atoms/text/text"
+import Button from "@components/UI/atoms/button/button"
+import LabeledInput from "@components/UI/blocks/labeledInput/labeledInput"
 import Theme from "@util/style/theme"
-
-const PaymentDetail = () => {
+import { useState } from "react"
+const PaymentDetail = (fullAddress, zoneCode) => {
+    const event1 = () => {}
+    const event2 = () => {}
+    const [srt, setSrt] = useState("")
     return (
         <CustomContainer width="50%" bColor={Theme.colors.whiteGray}>
-            <CustomContainer margin="0 0 2% 0">
-                <Text type="large" context="Payment Datails"></Text>
-            </CustomContainer>
-            <CustomContainer margin="0 0 3% 0">
-                <Text
-                    type="default"
-                    context="Complete your purchase by providing your payment details"
-                ></Text>
-            </CustomContainer>
-            <InputContainer>
-                <LabeledInput
-                    width="100%"
-                    labelText="Email address"
-                    inputType="text"
-                    placeholder="enter your email"
-                    name="email"
-                ></LabeledInput>
-                <LabeledInput
-                    width="100%"
-                    labelText="Name"
-                    inputType="text"
-                    placeholder="박재현 짱짱맨"
-                    name="name"
-                ></LabeledInput>
-                <LabeledInput
-                    width="100%"
-                    labelText="Phone number"
-                    inputType="text"
-                    placeholder="010-0000-0000"
-                    name="phoneNumber"
-                ></LabeledInput>
-                <GridContainer fr="2fr 1fr">
+            <CustomContainer margin="5% 5% 5% 5%">
+                <CustomContainer margin="0 0 2% 0">
+                    <Text type="large" context="Payment Datails"></Text>
+                </CustomContainer>
+                <CustomContainer margin="0 0 3% 0">
+                    <Text
+                        type="default"
+                        context="Complete your purchase by providing your payment details"
+                    ></Text>
+                </CustomContainer>
+                <InputContainer>
                     <LabeledInput
                         width="100%"
-                        labelText="Full address"
-                        readOnly
+                        labelText="Email address"
+                        inputType="text"
+                        placeholder="enter your email"
+                        name="email"
                     ></LabeledInput>
-                    <CustomContainer width="100%" margin="20px 0 0 0">
-                        <Button
-                            value="Address search"
+                    <LabeledInput
+                        width="100%"
+                        labelText="Name"
+                        inputType="text"
+                        placeholder="박재현 짱짱맨"
+                        name="name"
+                    ></LabeledInput>
+                    <LabeledInput
+                        width="100%"
+                        labelText="Phone number"
+                        inputType="text"
+                        placeholder="010-0000-0000"
+                        name="phoneNumber"
+                    ></LabeledInput>
+                    <GridContainer fr="2fr 1fr">
+                        <LabeledInput
                             width="100%"
+                            labelText="Full address"
                             name="fullAddress"
-                        ></Button>
-                    </CustomContainer>
-                </GridContainer>
-                <GridContainer fr="2fr 1fr">
+                            value={srt}
+                            onChangeEvent={event1}
+                            readOnly
+                        ></LabeledInput>
+                        <CustomContainer width="100%" margin="20px 0 0 0">
+                            <Button
+                                value="Address search"
+                                width="100%"
+                                name="fullAddressBtn"
+                                onClickEvent={event1}
+                            ></Button>
+                        </CustomContainer>
+                    </GridContainer>
+                    <GridContainer fr="2fr 1fr">
+                        <LabeledInput
+                            width="100%"
+                            labelText="Extra address"
+                            inputType="text"
+                            name="extraAddress"
+                        ></LabeledInput>
+                        <LabeledInput
+                            width="100%"
+                            labelText="ZoneCode"
+                            inputType="text"
+                            name="zoneCode"
+                            value={srt}
+                            onChangeEvent={event2}
+                            readOnly
+                        ></LabeledInput>
+                    </GridContainer>
                     <LabeledInput
                         width="100%"
-                        labelText="Extra address"
+                        labelText="Additional order"
+                        placeholder="Additional order"
                         inputType="text"
-                        name="extraAddress"
+                        name="additionalOrder"
                     ></LabeledInput>
-                    <LabeledInput
-                        width="100%"
-                        labelText="ZoneCode"
-                        inputType="text"
-                        name="zoneCode"
-                        readOnly
-                    ></LabeledInput>
-                </GridContainer>
-                <LabeledInput
+                </InputContainer>
+                <CustomContainer margin="2% 0 0 0">
+                    <GridContainer fr="6fr 1fr">
+                        <Label labelText="Products price"></Label>
+                        <Label labelText="here price props"></Label>
+                    </GridContainer>
+                </CustomContainer>
+                <CustomContainer margin="2% 0 0 0">
+                    <GridContainer fr="6fr 1fr">
+                        <Label labelText="Delivery price"></Label>
+                        <Label labelText="here price props"></Label>
+                    </GridContainer>
+                </CustomContainer>
+                <CustomContainer margin="2% 0 5% 0">
+                    <GridContainer fr="6fr 1fr">
+                        <Label labelText="Total price"></Label>
+                        <Label labelText="here price props"></Label>
+                    </GridContainer>
+                </CustomContainer>
+                <Button
                     width="100%"
-                    labelText="Additional order"
-                    inputType="text"
-                    name="additionalOrder"
-                    value="No Additional order"
-                ></LabeledInput>
-            </InputContainer>
-            <CustomContainer margin="2% 0 0 0">
-                <GridContainer fr="6fr 1fr">
-                    <Label labelText="Products price"></Label>
-                    <Label labelText="here price props"></Label>
-                </GridContainer>
+                    value="Pay!"
+                    onClickEvent={event1}
+                ></Button>
             </CustomContainer>
-            <CustomContainer margin="2% 0 0 0">
-                <GridContainer fr="6fr 1fr">
-                    <Label labelText="Delivery price"></Label>
-                    <Label labelText="here price props"></Label>
-                </GridContainer>
-            </CustomContainer>
-            <CustomContainer margin="2% 0 5% 0">
-                <GridContainer fr="6fr 1fr">
-                    <Label labelText="Total price"></Label>
-                    <Label labelText="here price props"></Label>
-                </GridContainer>
-            </CustomContainer>
-            <Button width="100%" value="Pay!"></Button>
         </CustomContainer>
     )
 }
--- src/components/UI/blocks/payment/paymentInfo.jsx
+++ src/components/UI/blocks/payment/paymentInfo.jsx
@@ -1,50 +1,71 @@
-import Container from "../../atoms/container/container"
 import styled from "styled-components"
-import Label from "../../atoms/label/label"
+import Label from "@components/UI/atoms/label/label"
+import Text from "@components/UI/atoms/text/text"
+import Input from "@components/UI/atoms/input/input"
+import Carousel from "@components/UI/blocks/carousel/carousel"
+import Container from "@components/UI/atoms/container/container"
 import Theme from "@/util/style/theme"
-import Carousel from "../carousel/carousel"
-import Text from "../../atoms/text/text"
-import Input from "../../atoms/input/input"
 const PaymentInfo = () => {
+    const event3 = () => {}
     const sampleItem = [
-        { id: 1, value: "value1" },
-        { id: 2, value: "value2" },
-        { id: 3, value: "value3" },
-        { id: 4, value: "value4" },
-        { id: 5, value: "value5" },
+        { id: 11, value: "value1" },
+        { id: 12, value: "value2" },
+        { id: 13, value: "value3" },
+        { id: 14, value: "value4" },
+        { id: 15, value: "value5" },
+        { id: 16, value: "value1" },
+        { id: 17, value: "value2" },
+        { id: 18, value: "value3" },
+        { id: 19, value: "value4" },
+        { id: 20, value: "value5" },
     ]
     return (
         <CustomContainer width="50%" bColor={Theme.colors.whiteGray}>
-            <CustomContainer margin="0 0 5% 0" padding="10px 0 0 0">
-                <GridContainer fr="0.2fr 2fr">
-                    <ProfileContiner>
-                        <ProfileImg
-                            src={`${process.env.PUBLIC_URL}/img/maincover.png`}
-                        ></ProfileImg>
-                    </ProfileContiner>
-                    <CustomContainer padding="0.8rem 0 0 0">
-                        <Label labelText="USER NAME"></Label>
-                    </CustomContainer>
-                </GridContainer>
-            </CustomContainer>
-            <CustomContainer>
-                <Label
-                    labelText={
-                        <Text type="large" context="Check Your Products"></Text>
-                    }
-                ></Label>
-            </CustomContainer>
-            <CustomContainer margin="5% 0 0 0">
-                <Carousel margin="0 0 10% 0"></Carousel>
-                <CustomContainer margin="0 0 10% 0">
-                    <Label labelText="아래는 당신이 고른 제품이다 체크를 해서 넣거나 뺄수있다 확인해라"></Label>
-                </CustomContainer>
-                {sampleItem.map((item) => (
-                    <GridContainer margin="0 0 10px 0" fr="6fr 1fr">
-                        <Input key={item.id} value={item.value}></Input>
-                        <Input key={item.id} type="checkbox"></Input>
+            <CustomContainer margin="5% 5% 5% 5%">
+                <CustomContainer margin="0 0 5% 0" padding="10px 0 0 0">
+                    <GridContainer fr="0.2fr 2fr">
+                        <ProfileContiner>
+                            <ProfileImg
+                                src={`${process.env.PUBLIC_URL}/img/maincover.png`}
+                            ></ProfileImg>
+                        </ProfileContiner>
+                        <CustomContainer padding="0.8rem 0 0 0">
+                            <Label labelText="USER NAME"></Label>
+                        </CustomContainer>
                     </GridContainer>
-                ))}
+                </CustomContainer>
+                <CustomContainer>
+                    <Label
+                        labelText={
+                            <Text
+                                type="large"
+                                context="Check Your Products"
+                            ></Text>
+                        }
+                    ></Label>
+                </CustomContainer>
+                <CustomContainer margin="5% 0 0 0">
+                    <Carousel margin="0 0 10% 0"></Carousel>
+                    <CustomContainer margin="0 0 10% 0">
+                        <Label labelText="아래는 당신이 고른 제품이다 체크를 해서 넣거나 뺄수있다 확인해라"></Label>
+                    </CustomContainer>
+                    <CustomContainer maxHeight="20rem" overFlow="scroll">
+                        {sampleItem.map((e) => (
+                            <CustomContainer
+                                key={e.id}
+                                display="grid"
+                                width="100%"
+                            >
+                                <StyledLabel>{e.value}</StyledLabel>
+                                <Input
+                                    key={e.id}
+                                    type="checkbox"
+                                    onChangeEvent={event3}
+                                ></Input>
+                            </CustomContainer>
+                        ))}
+                    </CustomContainer>
+                </CustomContainer>
             </CustomContainer>
         </CustomContainer>
     )
@@ -58,14 +79,26 @@ const CustomContainer = styled.div`
     background-color: ${(props) => props.bColor};
     max-height: ${(props) => props.maxHeight};
     overflow: ${(props) => props.overFlow};
+    dispaly: ${(props) => props.display};
 `
 const GridContainer = styled.div`
     display: grid;
+    width: ${(props) => props.width};
     height: ${(props) => props.height};
     margin: ${(props) => props.margin};
     grid-template-columns: ${(props) => props.fr || "1fr 1fr"};
 `
 
+const StyledLabel = styled.label`
+    outline: none;
+    width: 80%;
+    border: ${Theme.inputs.border};
+    border-radius: ${Theme.inputs.borderRadius};
+    box-sizing: ${Theme.inputs.boxSizing};
+    padding: ${Theme.inputs.padding};
+    display: ${Theme.inputs.display};
+    background-color: ${Theme.colors.white};
+`
 const ProfileContiner = styled.div`
     margin-left: 5%;
     width: 3rem;
