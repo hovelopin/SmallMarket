9cff9ab459dae0847d0996f84b9b5f09aa70a3c1
foxmon <jjockrod@naver.com>
2022-09-17 02:48:13 +0900

    Fix: App.jsx 수정

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,10 @@
 import AuthContext from "@/context/auth/authContext"
-import Header from "@components/UI/container/header"
 import CustomRouter from "@components/router/customRouter"
 import Footer from "@components/UI/container/footer"
 
 function App() {
     return (
         <AuthContext>
-            <Header />
             <CustomRouter />
             <Footer />
         </AuthContext>
