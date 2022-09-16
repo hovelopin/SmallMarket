96a43fb5b5fece72a311e20c89316cbabd53460b
foxmon <jjockrod@naver.com>
2022-09-17 02:09:19 +0900

    Fix: CustomRouter context 추가

--- src/components/router/customRouter.jsx
+++ src/components/router/customRouter.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import AuthContext from "@/context/auth/authContext"
 import Main from "@components/UI/container/main"
 import LoginContainer from "@components/UI/container/loginContainer"
 import RegisterContainer from "@components/UI/container/registerContainer"
@@ -10,18 +11,24 @@ import NotFound from "@components/UI/container/NotFound"
 
 const CustomRouter = () => {
     return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Main} />
-                <Route exact path="/login" component={LoginContainer} />
-                <Route exact path="/register" component={RegisterContainer} />
-                <Route exact path="/admin" component={AdminContainer} />
-                <Route exact path="/cart" component={CartContainer} />
-                <Route exact path="/items" component={ProductContainer} />
-                <Route exact path="/detail" component={DetailContainer} />
-                <Route component={NotFound} />
-            </Switch>
-        </Router>
+        <AuthContext>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Main} />
+                    <Route exact path="/login" component={LoginContainer} />
+                    <Route
+                        exact
+                        path="/register"
+                        component={RegisterContainer}
+                    />
+                    <Route exact path="/admin" component={AdminContainer} />
+                    <Route exact path="/cart" component={CartContainer} />
+                    <Route exact path="/items" component={ProductContainer} />
+                    <Route exact path="/detail" component={DetailContainer} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Router>
+        </AuthContext>
     )
 }
 
