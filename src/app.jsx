9f3991eb921f138af9ee04180cd966f31936f5ed
foxmon <jjockrod@naver.com>
2022-09-21 22:42:22 +0900

    Fix: App.jsx Fix

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,11 @@
 import React from "react"
-import AuthContext from "@/context/auth/authContext"
 import CustomRouter from "@components/router/customRouter"
 import Footer from "@components/UI/container/footer"
 
 function App() {
     return (
         <React.Fragment>
-            <AuthContext>
-                <CustomRouter />
-            </AuthContext>
+            <CustomRouter />
             <Footer />
         </React.Fragment>
     )
