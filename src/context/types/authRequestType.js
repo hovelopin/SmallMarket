0b4ac7677de124b603d382a2c38ae143be79922f
foxmon <jjockrod@naver.com>
2022-09-21 22:41:47 +0900

    Delete: 필요없는 파일 삭제

--- src/context/types/authRequestType.js
+++
@@ -1,8 +0,0 @@
-const AuthTypes = {
-    login: "REQUEST_USER_LOGIN",
-    current: "REQUEST_UESR_STATE",
-    logout: "REQUEST_USER_LOGOUT",
-}
-
-Object.freeze(AuthTypes)
-export default AuthTypes
