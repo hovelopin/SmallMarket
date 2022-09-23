867fb5bd9159bcaf76b08798e479fdecbb9442b0
hovelopin <kimhojin3714@naver.com>
2022-09-23 18:08:34 +0900

    feat : cart 페이지 pay로 라우팅 및 data 이동

--- src/components/UI/blocks/cart/cartSidebar.jsx
+++ src/components/UI/blocks/cart/cartSidebar.jsx
@@ -3,10 +3,28 @@ import Container from "@components/UI/atoms/container/container"
 import Text from "@components/UI/atoms/text/text"
 import Theme from "@/util/style/theme"
 import Button from "@components/UI/atoms/button/button"
+import AuthService from "@/service/authService"
+import { useHistory } from "react-router-dom"
+import CartService from "@/service/cartService"
 
 const CartSidebar = ({ totalPrice, discount, totalQuantity }) => {
     const discountedPrice = Math.floor(totalPrice / discount)
+    const userAuth = AuthService.firebaseCurrentUserReuqest()
+    const history = useHistory()
 
+    const handlePayBtnClick = async () => {
+        // cart 정보 불러오기
+        const cartItems = await CartService.firebaseCartInformationRequest(
+            userAuth.uid
+        )
+        // 페이지 이동 시켜주기 카트 데이터 담아서
+        history.push({
+            pathname: `/payment/${userAuth.uid}`,
+            state: {
+                ...cartItems,
+            },
+        })
+    }
     return (
         <Container width="100%">
             <CartSidebarMainContainer>
@@ -58,7 +76,12 @@ const CartSidebar = ({ totalPrice, discount, totalQuantity }) => {
                 </CartSidebarFooter>
             </CartSidebarMainContainer>
             <CartSidebarButton>
-                <Button type="default" width="100%" value="PAYMENT" />
+                <Button
+                    type="default"
+                    width="100%"
+                    value="PAYMENT"
+                    onClickEvent={handlePayBtnClick}
+                />
             </CartSidebarButton>
         </Container>
     )
