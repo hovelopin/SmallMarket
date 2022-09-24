f88ad67889f70510ebd7a98151a4df0e5899fc43
Velo248 <jhpark4884@gmail.com>
2022-09-24 23:14:14 +0900

    Fix: pagination기능 추가, limit옵션 선택 추가

--- src/components/UI/container/productContainer.jsx
+++ src/components/UI/container/productContainer.jsx
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from "react"
 import { useParams, useHistory } from "react-router-dom"
 import styled from "styled-components"
+import Select from "@components/UI/atoms/select/select"
 import Container from "@components/UI/atoms/container/container"
 import Carousel from "@components/UI/blocks/carousel/carousel"
-import Grid from "@components/UI/atoms/grid/grid"
-import Card from "@components/UI/blocks/card/card"
+import Paginate from "@components/UI/blocks/paginate/paginate"
 import ProductService from "@/service/productService"
 import Theme from "@util/style/theme"
 
 const ProductContainer = () => {
     const [items, setItems] = useState([])
-
+    const [limit, setLimit] = useState(12)
+    const [page, setPage] = useState(1)
     const { category } = useParams()
-
     const history = useHistory()
-
+    const options = ["8개씩 보기", "12개씩 보기", "16개씩 보기"]
     useEffect(async () => {
         const selectedItems = await ProductService.firebaseGetCategoryRequest(
             category
@@ -22,7 +22,7 @@ const ProductContainer = () => {
         setItems(selectedItems)
     }, [category])
 
-    const handleDetailButtonClick = (item) => () => {
+    const handleDetailButtonClick = (item) => {
         history.push({
             pathname: `/detail/${item.uuid}`,
             state: {
@@ -31,6 +31,10 @@ const ProductContainer = () => {
         })
     }
 
+    const handleLimitChange = (e) => {
+        const newLimit = parseInt(e.target.value.replace(/[^0-9]/g, ""))
+        setLimit(newLimit)
+    }
     return (
         <Container width="100%">
             <StyledItemHeaderContainer>
@@ -42,30 +46,29 @@ const ProductContainer = () => {
             <StyledCarouselContainer>
                 <Carousel />
             </StyledCarouselContainer>
-            <StyledContainer width="100%">
-                <Grid repeat={4} axis="column" gap="2rem">
-                    {items.map((item) => (
-                        <Card
-                            key={item.uuid}
-                            img={item.img}
-                            uuid={item.uuid}
-                            name={item.name}
-                            price={item.price}
-                            quantity={item.quantity}
-                            onClickEvent={handleDetailButtonClick(item)}
-                        />
-                    ))}
-                </Grid>
+            <StyledContainer>
+                <Select
+                    options={options}
+                    onChangeEvent={handleLimitChange}
+                    defaultSelected="12개씩 보기"
+                ></Select>
             </StyledContainer>
+            <Paginate
+                items={items}
+                limit={limit}
+                total={items.length}
+                onDetailButtonClickEvent={handleDetailButtonClick}
+                page={page}
+                setPage={setPage}
+            ></Paginate>
         </Container>
     )
 }
 
 const StyledContainer = styled.div`
-    width: 100%;
-    display: grid;
-    margin-top: 3rem;
-    place-items: center;
+    width: 10%;
+    margin-top: 3%;
+    margin-left: 80%;
 `
 
 const StyledItemHeaderContainer = styled.div`
