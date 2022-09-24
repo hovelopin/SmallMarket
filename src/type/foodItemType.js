8eed64f3c64d77f030fbcf55d8ddc9fbe5b1e980
foxmon <jjockrod@naver.com>
2022-09-24 17:33:00 +0900

    Fix: FoodItemType 수정

--- src/type/foodItemType.js
+++ src/type/foodItemType.js
@@ -1,5 +1,6 @@
-import Category from "../util/category"
-import Item from "./item"
+import Item from "@/type/item"
+import Category from "@util/category"
+import ErrorUtil from "@util/errorUtil"
 
 const FoodItemType = {}
 
@@ -97,6 +98,71 @@ FoodItemType.createNormal = function (
         Category.N,
         "NormalItemType"
     )
+}
+
+FoodItemType.craeteType = function (productItemData) {
+    const {
+        uuid,
+        name,
+        description,
+        price,
+        quantity,
+        img,
+        sellerUuid,
+        origin,
+    } = productItemData
+    switch (productItemData.category) {
+        case "Drink":
+            return FoodItemType.createDrink(
+                uuid,
+                name,
+                description,
+                price,
+                quantity,
+                img,
+                sellerUuid,
+                origin
+            )
+
+        case "Meat":
+            return FoodItemType.createMeat(
+                uuid,
+                name,
+                description,
+                price,
+                quantity,
+                img,
+                sellerUuid,
+                origin
+            )
+
+        case "Vegetable":
+            return FoodItemType.createVegetable(
+                uuid,
+                name,
+                description,
+                price,
+                quantity,
+                img,
+                sellerUuid,
+                origin
+            )
+
+        case "Normal":
+            return FoodItemType.createNormal(
+                uuid,
+                name,
+                description,
+                price,
+                quantity,
+                img,
+                sellerUuid,
+                origin
+            )
+
+        default:
+            ErrorUtil.notImplemented()
+    }
 }
 
 Object.freeze(FoodItemType)
