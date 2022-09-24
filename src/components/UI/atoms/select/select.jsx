aa7ecc8be82ab88ddf8587c5672d8f46da46182f
Velo248 <jhpark4884@gmail.com>
2022-09-24 23:09:03 +0900

    Fix: atoms select defaultSelected추가

--- src/components/UI/atoms/select/select.jsx
+++ src/components/UI/atoms/select/select.jsx
@@ -1,10 +1,14 @@
 import * as Selector from "@components/UI/atoms/select/syle"
 
-const Select = ({ width, options, onChangeEvent }) => {
+const Select = ({ width, options, defaultSelected, onChangeEvent }) => {
     return (
         <Selector.StyledSelect width={width} onChange={onChangeEvent}>
             {options.map((value) => (
-                <Selector.StyledOption key={value} value={value}>
+                <Selector.StyledOption
+                    key={value}
+                    value={value}
+                    selected={value === defaultSelected}
+                >
                     {value}
                 </Selector.StyledOption>
             ))}
