2dae512201bf3917dfda0ed7813d35f2e39112d5
foxmon <jjockrod@naver.com>
2022-09-24 17:33:28 +0900

    Fix: AuthService 수정

--- src/service/authService.js
+++ src/service/authService.js
@@ -68,7 +68,16 @@ AuthService.firebaseRegiserRequest = async function (
                 { merge: true }
             )
         }
-
+        const cartDoc = await addDoc(collection(firestore, "cart"), {
+            userUuid: user.uid,
+        })
+        setDoc(
+            doc(firestore, "cart", `${cartDoc.id}`),
+            {
+                uuid: cartDoc.id,
+            },
+            { merge: true }
+        )
         return user
     } catch (e) {
         return e
