a0a67bddadbcd4a70c9614b88389f8824e82c2c6
foxmon <jjockrod@naver.com>
2022-09-24 17:33:14 +0900

    Fix: ProductService 수정

--- src/service/productService.js
+++ src/service/productService.js
@@ -1,21 +1,31 @@
-import { doc, addDoc, setDoc, getDocs, collection } from "firebase/firestore"
+import {
+    doc,
+    addDoc,
+    setDoc,
+    getDocs,
+    collection,
+    query,
+    where,
+} from "firebase/firestore"
 import { firestore } from "@/service/firebaseService"
 import ImageService from "@/service/imageService"
 import FoodItemType from "@/type/foodItemType"
 import FoodItemListype from "@/type/foodItemListType"
-import Data from "@/dev/data"
-import ErrorUtil from "@util/errorUtil"
 
 const ProductService = {}
 
 ProductService.type = "ProductServiceType"
 
 ProductService.firebaseAddToCartRequeset = async function (item, userUuid) {
-    if (Data?.cart[userUuid]?.includes(item)) return false
-    Data.cart[userUuid]
-        ? Data.cart[userUuid].push(item)
-        : (Data.cart[userUuid] = [item])
-    return true
+    const cartDocs = collection(firestore, "cart")
+    const cartQuery = query(cartDocs, where("userUuid", "==", `${userUuid}`))
+    const carts = await getDocs(cartQuery)
+    const cartArray = []
+    carts.docs.forEach((cart) => {
+        cartArray(cart)
+    })
+    const res = cartArray.find((cart) => cart.productUuid === item.uuid)
+    if (res) return false
 }
 
 ProductService.firebaseAddProductRequest = async function (
@@ -62,78 +72,15 @@ ProductService.firebaseGetCategoryRequest = async function (categoryType) {
     const categoryItems = []
 
     productDocs.forEach((p) => {
-        categoryItems.push(craeteType(toUpper, p.data()))
+        categoryItems.push(FoodItemType.craeteType(p.data()))
     })
-    const selectedItems = categoryItems.filter(
-        (c) => c.category.category === toUpper
-    )
+    const selectedItems = categoryItems.filter((c) => {
+        if (c.category.category.toUpperCase() === toUpper.toUpperCase()) {
+            return c
+        }
+    })
     return FoodItemListype.createFoodItemListType(selectedItems)
         .$_foodItemListType
-}
-
-function craeteType(toUpper, productItemData) {
-    const {
-        uuid,
-        name,
-        description,
-        price,
-        quantity,
-        img,
-        sellerUuid,
-        origin,
-    } = productItemData
-    switch (toUpper) {
-        case "Drink":
-            return FoodItemType.createDrink(
-                uuid,
-                name,
-                description,
-                price,
-                quantity,
-                img,
-                sellerUuid,
-                origin
-            )
-
-        case "Meat":
-            return FoodItemType.createMeat(
-                uuid,
-                name,
-                description,
-                price,
-                quantity,
-                img,
-                sellerUuid,
-                origin
-            )
-
-        case "Vegetable":
-            return FoodItemType.createVegetable(
-                uuid,
-                name,
-                description,
-                price,
-                quantity,
-                img,
-                sellerUuid,
-                origin
-            )
-
-        case "Normal":
-            return FoodItemType.createNormal(
-                uuid,
-                name,
-                description,
-                price,
-                quantity,
-                img,
-                sellerUuid,
-                origin
-            )
-
-        default:
-            ErrorUtil.notImplemented()
-    }
 }
 
 Object.freeze(ProductService)
